b3ed763671436d296f189f5989f3df57fb74f074
Shreyas Uttekar <55836478+UttekarShreyas007@users.noreply.github.com>
2023-05-01 03:44:51 +0530

    Update app.js expose headers

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://uttekarsrealty.netlify.app');
   res.header('Access-Control-Allow-Credentials', true);
+   res.header('Access-Control-Expose-Headers', *);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin');
   next();
