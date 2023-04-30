8ce61d454a8bb052755717d38f497b6ba40ac937
Shreyas Uttekar <55836478+UttekarShreyas007@users.noreply.github.com>
2023-05-01 02:33:41 +0530

    Update auth.js samesite setting in login

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res, next) => {
         role: user.role
       }
       req.session.token = token;
-      res.cookie("token", token, {maxAge: 36000000});
+      res.cookie("token", token, {maxAge: 36000000, sameSite: 'none', secure: true});
       res.send({message: "Login success", userObj});
     });
   } catch (error) {
