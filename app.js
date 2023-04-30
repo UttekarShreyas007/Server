a9ba802eb643527070b7b487cb793bc15e99b0c3
Shreyas Uttekar <55836478+UttekarShreyas007@users.noreply.github.com>
2023-04-30 22:29:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://uttekarsrealty.netlify.app/');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
