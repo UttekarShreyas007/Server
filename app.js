4cb204e74ac0aa6fe36ff123a9093dc34f590a64
Shreyas Uttekar <55836478+UttekarShreyas007@users.noreply.github.com>
2023-05-01 13:05:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
    res.header('Access-Control-Expose-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin, Access-Control-Expose-Headers');
   next();
 });
 // Connect to MongoDB
