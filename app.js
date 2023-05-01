7a10774974ed64380c7491d0714ffb1db9076d81
Shreyas Uttekar <55836478+UttekarShreyas007@users.noreply.github.com>
2023-05-01 23:36:52 +0530

    token added server

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-const config = require('./config');
-const authMiddleware = require('./middleware/authMiddleware');
-const errorHandler = require('./middleware/errorHandler');
-const propertyRoutes = require('./routes/properties');
-const userRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');  
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const config = require("./config");
+const authMiddleware = require("./middleware/authMiddleware");
+const errorHandler = require("./middleware/errorHandler");
+const propertyRoutes = require("./routes/properties");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
@@ -19,53 +19,58 @@ const app = express();
 // var whitelist = ["https://uttekarsrealty.netlify.app", "http://localhost:3000"]
 app.use(
   cors({
-    origin: "https://uttekarsrealty.onrender.com",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
-    preflightContinue: true,
   })
 );
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://uttekarsrealty.onrender.com');
-  res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Expose-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin, Access-Control-Expose-Headers');
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Expose-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Access-Control-Allow-Origin, Access-Control-Expose-Headers"
+  );
   next();
 });
 // Connect to MongoDB
-mongoose.connect(config.mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(config.mongodbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB', process.env.RENDER_EXTERNAL_HOSTNAME);
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error);
+    console.log("Error connecting to MongoDB:", error);
   });
 
-  const store = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
-    collection: "sessions",
-  });
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: "sessions",
+});
 
-  app.use(
-    session({
-      secret: "iuZeL0LrnkO5K$edr3RWD!S!q",
-      resave: false,
-      saveUninitialized: false,
-      store: store, 
-      cookie: {
-        maxAge: 36000000, //3600000ms = 1hr
-        sameSite: 'none',
-        secure: true
-      },
-    })
-  );
-  
- 
-  app.use(bodyParser.json());
-  app.use(morgan('dev'));
-  app.use(cookieParser());
-  
+app.use(
+  session({
+    secret: "iuZeL0LrnkO5K$edr3RWD!S!q",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      maxAge: 36000000, //3600000ms = 1hr
+      sameSite: "none",
+      secure: true,
+    },
+  })
+);
+
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
 // Use middleware
 // Authorization middleware
 app.use(authMiddleware);
@@ -73,21 +78,16 @@ app.use(authMiddleware);
 // Error handling middleware
 app.use(errorHandler);
 
-
-
 // Use routes
-app.use('/api/properties', propertyRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-
-
-
+app.use("/api/properties", propertyRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,56 +1,61 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const config = require('../config');
-const User = require('../models/User');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const User = require("../models/User");
 
 const router = express.Router();
 
 // User signup route
-router.post('/signup', async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(409).json({ message: 'User already exists' });
+      return res.status(409).json({ message: "User already exists" });
     }
     const user = new User({ name, email, password });
     await user.save();
     const token = jwt.sign({ userId: user._id }, config.jwtSecret, {
-      expiresIn: '1d'
+      expiresIn: "1d",
     });
-    res.status(201).json({ message: 'User created successfully', token });
+    res.status(201).json({ message: "User created successfully", token });
   } catch (error) {
     next(error);
   }
 });
 
 // User login route
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(401).json({ message: 'User not found' });
+      return res.status(401).json({ message: "User not found" });
     }
 
     user.comparePassword(password, function (err, isMatch) {
       if (err) throw err;
       if (!isMatch) {
-        return res.status(401).json({ message: 'Authentication failed' });
+        return res.status(401).json({ message: "Authentication failed" });
       }
       const token = jwt.sign({ userId: user._id }, config.jwtSecret, {
-        expiresIn: '1d'
+        expiresIn: "1d",
       });
       const userObj = {
         name: user.name,
         email: user.email,
         _id: user._id,
-        role: user.role
-      }
+        role: user.role,
+      };
       req.session.token = token;
-      res.cookie("token", token, {maxAge: 36000000, sameSite: 'none', secure: true, domain: 'real-estate-web-app.onrender.com', httpOnly: false});
-      res.send({message: "Login success", userObj});
+      res.cookie("token", token, {
+        maxAge: 36000000,
+        sameSite: "none",
+        secure: true,
+        httpOnly: false,
+      });
+      res.send({ message: "Login success", userObj, token: token });
     });
   } catch (error) {
     next(error);
